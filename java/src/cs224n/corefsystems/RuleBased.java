cd81c139f4cc1a8dea8bc71d93a6d5594af938eb
Anand Sampat <asampat3090@gmail.com>
2014-11-04 12:02:00 -0800

    RuleBased - try exact head maching

--- java/src/cs224n/corefsystems/RuleBased.java
+++ java/src/cs224n/corefsystems/RuleBased.java
@@ -1,11 +1,15 @@
 package cs224n.corefsystems;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
 import cs224n.coref.Entity;
+import cs224n.coref.Mention;
 import cs224n.util.Pair;
 
 public class RuleBased implements CoreferenceSystem {
@@ -13,13 +17,34 @@ public class RuleBased implements CoreferenceSystem {
 	@Override
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
 		// TODO Auto-generated method stub
+		// Do nothing - unless statistics needed
 
 	}
 
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
 		// TODO Auto-generated method stub
-		return null;
+		List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
+		Map<String,Entity> clusters = new HashMap<String,Entity>();
+		// Exact head matching 
+		//(for each mention...)
+		for(Mention m : doc.getMentions()){
+			//(...get its text)
+			String mentionStringHead = m.headWord();
+			//(...if we've seen the head of this text before...)
+			if(clusters.containsKey(mentionStringHead)){
+				//(...add it to the cluster)
+				mentions.add(m.markCoreferent(clusters.get(mentionStringHead)));
+			} else {
+				//(...else create a new singleton cluster)
+				ClusteredMention newCluster = m.markSingleton();
+				mentions.add(newCluster);
+				clusters.put(mentionStringHead,newCluster.entity);
+			}
+		}
+		//(return the mentions)
+		return mentions;
+		
 	}
 
 }
