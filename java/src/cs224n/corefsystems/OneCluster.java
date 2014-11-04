8f0f67c18efaee72f20244eef9d023139d31bdda
Anand Sampat <asampat3090@gmail.com>
2014-11-03 20:08:28 -0800

    tried one cluster

--- java/src/cs224n/corefsystems/OneCluster.java
+++ java/src/cs224n/corefsystems/OneCluster.java
@@ -1,11 +1,13 @@
 package cs224n.corefsystems;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
 import cs224n.coref.Entity;
+import cs224n.coref.Mention;
 import cs224n.util.Pair;
 
 public class OneCluster implements CoreferenceSystem {
@@ -19,7 +21,14 @@ public class OneCluster implements CoreferenceSystem {
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
 		// TODO Auto-generated method stub
-		return null;
+		List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
+		// Assign each mention to the one entity
+		// Create entity to add all mentions to
+		Entity oneEnt = new Entity(doc.getMentions());
+		for(Mention m : doc.getMentions()) {
+			mentions.add(m.markCoreferent(oneEnt));
+		}
+		return mentions;
 	}
 
 }
