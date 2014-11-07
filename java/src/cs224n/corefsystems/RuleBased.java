e4f3a76c294455670dd7def336fcea657c10403c
Anand Sampat <asampat3090@gmail.com>
2014-11-06 19:12:24 -0800

    small change case insensitive

--- java/src/cs224n/corefsystems/RuleBased.java
+++ java/src/cs224n/corefsystems/RuleBased.java
@@ -26,13 +26,13 @@ public class RuleBased implements CoreferenceSystem {
 		// TODO Auto-generated method stub
 		List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
 		Map<String,Entity> clusters = new HashMap<String,Entity>();
-		// Exact head matching 
+		// Step 1 : Exact head matching 
 		//(for each mention...)
 		for(Mention m : doc.getMentions()){
 			//(...get its text)
 			String mentionStringHead = m.headWord();
 			//(...if we've seen the head of this text before...)
-			if(clusters.containsKey(mentionStringHead)){
+			if(clusters.containsKey(mentionStringHead)||(clusters.containsKey(mentionStringHead.toLowerCase()))){
 				//(...add it to the cluster)
 				mentions.add(m.markCoreferent(clusters.get(mentionStringHead)));
 			} else {
@@ -42,6 +42,10 @@ public class RuleBased implements CoreferenceSystem {
 				clusters.put(mentionStringHead,newCluster.entity);
 			}
 		}
+		// Step 2: Gender coherence for pronouns
+		// reallocate mentions to new clusters if needed
+		
+		
 		//(return the mentions)
 		return mentions;
 		
