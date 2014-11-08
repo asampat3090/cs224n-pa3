74bc247b5505e563efe1a4ff377b2270aa86ff2a
Anand Sampat <asampat3090@gmail.com>
2014-11-07 22:29:30 -0800

    additive rules

--- java/src/cs224n/corefsystems/RuleBased.java
+++ java/src/cs224n/corefsystems/RuleBased.java
@@ -5,12 +5,14 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.lang.Math;
 
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
 import cs224n.coref.Entity;
 import cs224n.coref.Mention;
 import cs224n.coref.Util;
+import cs224n.coref.Pronoun;
 import cs224n.util.Pair;
 
 public class RuleBased implements CoreferenceSystem {
@@ -27,26 +29,64 @@ public class RuleBased implements CoreferenceSystem {
 		// TODO Auto-generated method stub
 		List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
 		Map<String,Entity> clusters = new HashMap<String,Entity>();
-		// Step 1 : Exact head matching 
-		//(for each mention...)
-		for(Mention m : doc.getMentions()){
-			//(...get its text)
-			String mentionStringHead = m.headWord();
-			//(...if we've seen the head of this text before...)
-			if(clusters.containsKey(mentionStringHead)||(clusters.containsKey(mentionStringHead.toLowerCase()))){
-				//(...add it to the cluster)
-				if((clusters.containsKey(mentionStringHead.toLowerCase()))) {
-					mentions.add(m.markCoreferent(clusters.get(mentionStringHead.toLowerCase())));
-				} else {
-					mentions.add(m.markCoreferent(clusters.get(mentionStringHead)));
+		
+		// Using sieve process - high precision to low precision
+		
+		// Additive rules //
+		
+		// Step 0 : Case insensitive string match
+	    //(for each mention...)
+	    for(Mention m : doc.getMentions()){
+	      //(...get its text)
+	      String mentionString = m.gloss();
+	      //(...if we've seen this text before...)
+	      if(clusters.containsKey(mentionString.toLowerCase())){
+	        //(...add it to the cluster)
+	        mentions.add(m.markCoreferent(clusters.get(mentionString.toLowerCase())));
+	      } else {
+	        //(...else create a new singleton cluster)
+	        ClusteredMention newCluster = m.markSingleton();
+	        mentions.add(newCluster);
+	        clusters.put(mentionString.toLowerCase(),newCluster.entity);
+	      }
+	    }			
+		
+		// Step 1 : Case insensitive head matching 
+		// Loop through each of the clustered mentions (= # mentions in doc)
+		for(ClusteredMention cm : mentions){
+			// Loop through each of the clusters
+			for (Map.Entry<String, Entity> entry : clusters.entrySet()) {
+				Entity cluster = entry.getValue();
+				// check if head word matches any of the mentions in the cluster
+				for(Mention mention : cluster.mentions) {
+					// Re cluster current clustered mention to new entity if matches case insensitive head word in entity
+					if(cm.mention.headWord().toLowerCase().equals(mention.headWord().toLowerCase())) {
+						cm=cm.mention.markCoreferent(cluster);
+						break;
+					}
 				}
-			} else {
-				//(...else create a new singleton cluster)
-				ClusteredMention newCluster = m.markSingleton();
-				mentions.add(newCluster);
-				clusters.put(mentionStringHead,newCluster.entity);
 			}
 		}
+		
+		// Step 2 : Case insensitive word inclusion
+		// Loop through each of the clustered mentions(= # mentions in doc)
+		for(ClusteredMention cm : mentions){
+			// Loop through each of the clusters
+			for (Map.Entry<String, Entity> entry : clusters.entrySet()) {
+				Entity cluster = entry.getValue();
+				// check if head word matches any of the mentions in the cluster
+				for(Mention mention : cluster.mentions) {
+					// Re cluster current clustered mention to new entity if matches a head word in entity
+					if(cm.mention.headWord().toLowerCase().contains(mention.headWord().toLowerCase())) {
+						cm=cm.mention.markCoreferent(cluster);
+						break;
+					}
+				}
+			}
+		}
+		
+		// Subtractive Rules //
+		
 		// Step 2: Gender and number consistency 
 		// reallocate if mention and entity have different 
 		for(ClusteredMention cm: mentions ) {
@@ -65,10 +105,23 @@ public class RuleBased implements CoreferenceSystem {
 		}
 		
 		// Step 3: Pronouns rules 
-		// Check pronoun distance 
-		//for(ClusteredMention cm: mentions) {
-		
-		//}
+		// Check pronoun sentence distance <3  
+		for(ClusteredMention cm: mentions) {
+			// check if mention is pronoun - then apply constraint 
+			if(Pronoun.isSomePronoun(cm.mention.headWord())) {
+				int pronounSentInd = doc.indexOfSentence(cm.mention.sentence); 
+				for(Mention m:cm.entity.mentions) {
+					int tempLen = doc.indexOfSentence(m.sentence);
+					if(Math.abs(tempLen-pronounSentInd)>3) {
+						// Remove coreference 
+						cm.mention.removeCoreference();
+						// Allocate mention to singleton cluster
+						//mentions.add(cm.mention.markSingleton());
+						cm.mention.markSingleton();
+					}
+				}
+			}
+		}
 		
 		// Step 4: Named Entity Rules
 		
